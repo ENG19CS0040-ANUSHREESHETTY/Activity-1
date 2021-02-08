e32c731459d58fa890ae15c1b8a20bbb42074995
ENG19CS0040-ANUSHREESHETTY <76063480+ENG19CS0040-ANUSHREESHETTY@users.noreply.github.com>
2021-02-08 22:17:11 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,11 @@
 //Write a program to find the sum of n different numbers using 4 functions
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+printf(“enter number:”)
+scanf(“%d”,&a);
+printf(“enter number:”)
+scanf(“%d”,&b);
+printf(“sum of a and b is %d”,sum);
+}
