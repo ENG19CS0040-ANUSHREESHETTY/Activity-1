e3ce6041ce46beb4ddb9421f3f0ab355a974ef02
ENG19CS0040-ANUSHREESHETTY <76063480+ENG19CS0040-ANUSHREESHETTY@users.noreply.github.com>
2021-02-10 09:52:09 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -4,9 +4,11 @@ int main()
 {
 int a,b,sum;
 printf(“enter number:”)
-scanf(“%d”,&a);
+ scanf(“%d”,&a);
 printf(“enter number:”)
-scanf(“%d”,&b);
+ scanf(“%d”,&b);
+sum=a+b;
 printf(“sum of a and b is %d”,sum);
+return(0);
 }
 
