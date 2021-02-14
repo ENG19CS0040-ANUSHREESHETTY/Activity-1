d0a0e72146efcbd606aec8e88670b87612504c5e
ENG19CS0040-ANUSHREESHETTY <76063480+ENG19CS0040-ANUSHREESHETTY@users.noreply.github.com>
2021-02-14 21:17:15 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,30 @@
-//WAP to find the volume of a tromboloid using 4 functions.
+//WAP to find the volume of a tromboloid using 4 functions.#include<stdio.h>
+float input()
+{
+    float a;
+    printf(“enter a number\n”);
+    scanf(“%f”,&a);
+    return a;
+}
+float find_vol(float a,float b,float c)
+{
+     float vol;
+     vol=⅓*((a*b*c)+(b/c));
+     return vol;
+}
+void output(float a)
+{
+printf(“the result is %f”,a);
+}
+int main()
+{
+    float x,y,z,vol;
+    x=input();
+    y=input();
+    z=input();
+    vol=find_vol(x,y,z);
+    output(vol);
+    return(0);
+}
+
+
