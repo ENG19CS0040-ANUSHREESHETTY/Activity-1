fbfd5c64606d3f743923430e550ef9063adb6174
ENG19CS0040-ANUSHREESHETTY <76063480+ENG19CS0040-ANUSHREESHETTY@users.noreply.github.com>
2021-02-14 19:29:06 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -2,10 +2,10 @@
 #include<stdio.h>
 int main()
 {
-float h,d,b,vol;
-printf(“enter the values of h,b and d\n”);
-scanf(“%f%f%f”,&h,&d,&b);
-vol=((h*d*b)+(d/b))/3.0;
-printf(“Volume of the tromboloid is=%f\n”,vol);
+  float h,d,b,vol;
+  printf(“enter the values of h,b and d\n”);
+   scanf(“%f%f%f”,&h,&d,&b);
+  vol=((h*d*b)+(d/b))/3.0;
+  printf(“Volume of the tromboloid is=%f\n”,vol);
 return(0);
 }
