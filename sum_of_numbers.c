0457047ca7f4f6db4d344902aa8bf65e94de1e2e
ENG19CS0040-ANUSHREESHETTY <76063480+ENG19CS0040-ANUSHREESHETTY@users.noreply.github.com>
2021-03-31 23:05:35 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -2,41 +2,41 @@
 #include<stdio.h>
 int input()
 {
-int a;
-printf("Enter a number: \n");
-scanf("%d", &a);
-return a;
+  int a;
+  printf("Enter a number: \n");
+  scanf("%d", &a);
+  return a;
 }
 void input_array(int n, int a[n])
 {
-for(int i=0;i<n;i++)
+  for(int i=0;i<n;i++)
 {
-printf("Enter the element number %d of the array: \n", i);
-scanf("%d", &a[i]);
+  printf("Enter the element number %d of the array: \n", i);
+  scanf("%d", &a[i]);
 }
 }
 int nsum(int n, int a[n])
 {
-int sum=0;
-for(int i=0;i<n;i++)
+  int sum=0;
+  for(int i=0;i<n;i++)
 {
-sum += a[i];
+  sum += a[i];
 }
 return sum;
 }
 int result(int n, int a[n], int sum)
 {
-int i;
-printf("The sum of %d different numbers is ", n);
-printf("%d", sum);
+  int i;
+  printf("The sum of %d different numbers is ", n);
+  printf("%d", sum);
 }
 int main()
 {
-int n,sum;
-n=input();
-int a[n];
-input_array(n,a);
-sum = nsum(n,a);
-result(n,a,sum);
+  int n,sum;
+  n=input();
+  int a[n];
+  input_array(n,a);
+  sum = nsum(n,a);
+  result(n,a,sum);
 }
 
