58717ef8b699b10c73a5e0f2bddc4a8682bc9ba3
ENG19CS0040-ANUSHREESHETTY <76063480+ENG19CS0040-ANUSHREESHETTY@users.noreply.github.com>
2021-04-15 22:00:20 +0530

    Update activity 13.c

--- activity 13.c
+++ activity 13.c
@@ -1,83 +1,83 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
-
-typedef struct coord
+struct points
 {
   float x;
   float y;
-
-} coordinate;
-
-typedef struct rect
+};
+typedef struct points point;
+struct rectangles
 {
-  coordinate P1, P2, P3;
-  float area, length, breadth;
-} rectangle;
+  point a[3];
+  float area;
+};
+typedef struct rectangles rectangle;
 
+void
+input (rectangle * one)
+{
+  for (int i = 0; i < 3; i++)
+    {
+      printf ("enter the value of point1\n");
+      scanf ("%f", &one->a[i].x);
+      printf ("enter the value of point2\n");
+      scanf ("%f", &one->a[i].y);
+    }
+}
 
-int input ()
+void
+input_n (rectangle a[], int n)
+{
+  for (int i = 0; i < n; i++)
+    input (&a[i]);
+}
+
+void
+compute (rectangle * one)
+{
+  float length =
+    sqrt (pow ((one->a[1].y - one->a[0].y), 2) +
+	  pow ((one->a[1].x - one->a[0].x), 2));
+  float breadth =
+    sqrt (pow ((one->a[0].y - one->a[2].y), 2) +
+	  pow ((one->a[0].x - one->a[2].x), 2));
+  one->area = length * breadth;
+}
+
+void
+compute_n (rectangle a[], int n)
+{
+  for (int i = 0; i < n; i++)
+    compute (&a[i]);
+}
+
+void
+output (rectangle * one)
+{
+  printf ("Area of rectangle with vertices ");
+  for (int i = 0; i < 3; i++)
+    printf ("(%.1f,%.1f) ", one->a[i].x, one->a[i].y);
+  printf ("is %.1f\n", one->area);
+}
+
+void
+output_n (rectangle a[], int n)
+{
+  for (int i = 0; i < n; i++)
+    output (&a[i]);
+}
+
+int
+main ()
 {
   int n;
-  printf ("Enter the number of test cases");
+  rectangle a[100];
+  printf ("Enter the number of rectangles to be analyzed:");
   scanf ("%d", &n);
-  return n;
-}
-
-void input_one (rectangle * p)
-{
-
-  printf ("enter the coordinates");
-  scanf ("%f %f %f %f %f %f  ", &p->P1.x, &p->P1.y, &p->P2.x, &p->P2.y,
-	 &p->P3.x, &p->P3.y);
-}
-
-void input_n (int n, rectangle arr[])
-{
-  for (int i = 0; i < n; i++)
-    {
-      input_one (&arr[i]);
-    }
-}
-
-void compute_one (rectangle * p)
-{
-  p->length =
-    sqrt (pow ((p->P1.x - p->P2.x), 2) + pow ((p->P1.y - p->P2.y), 2));
-  p->breadth =
-    sqrt (pow ((p->P1.x - p->P3.x), 2) + pow ((p->P1.y - p->P3.y), 2));
-  p->area = p->length * p->breadth;
-}
-
-
-void compute_n (int n, rectangle arr[])
-{
-  for (int i = 0; i < n; i++)
-    {
-      compute_one (&arr[i]);
-    }
-}
-
-void output_one (rectangle * p)
-{
-
-  printf (" Area of rectangle with vertices (%f,%f),(%f,%f),(%f,%f) is %f \n",
-	  p->P1.x, p->P1.y, p->P2.x, p->P2.y, p->P3.x, p->P3.y, p->area);
-}
-
-void output_n (int n, rectangle arr[])
-{
-  for (int i = 0; i < n; i++)
-    {
-      output_one (&arr[i]);
-    }
-}
-
-int main ()
-{
-  int n = input ();
-  rectangle arr[n];
-  input_n (n, arr);
-  compute_n (n, arr);
-  output_n (n, arr);
+  input_n (a, n);
+  compute_n (a, n);
+  output_n (a, n);
   return 0;
 }
+
